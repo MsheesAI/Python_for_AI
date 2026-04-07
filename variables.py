a3b5cd45fdfdabfef3583491c3909854b7e65ab5
MsheesAI <shees7795@gmail.com>
2026-04-07 13:14:38 +0500

    variables.py

---
+++ variables.py
@@ -0,0 +1,16 @@
+# What are Variables!
+# A Variable is a container for storing data Think of it like box
+
+name = "shees"
+first_name = "shees"
+firstName = "shees"
+
+#print is used for print value in the terminal
+
+print(name)
+print(first_name)
+print(firstName)
+
+a,b,c = ("shafayee","shees","yagami")
+print(a)
+
