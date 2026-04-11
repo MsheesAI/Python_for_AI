7f7f7a1b0febb724c08a8b50110582816da85e23
MsheesAI <shees7795@gmail.com>
2026-04-11 15:18:38 +0500

    add operators

---
+++ operators.py
@@ -0,0 +1,19 @@
+#Mathematical
+print(2+2)
+print(0-1)
+
+print(2*6)
+print(15//3)
+
+print(10%3)
+print(2**3)
+
+print(5==4)
+print(7!=5)
+print(10>2)
+print(4<1)
+
+print("______________")
+print(True and False)
+print(True or False)
+print(not False)
