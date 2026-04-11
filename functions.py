36b0c8bb453d472dee12633f19baea15fcf84725
MsheesAI <shees7795@gmail.com>
2026-04-11 16:04:06 +0500

    ADD lAMBDA

--- functions.py
+++ functions.py
@@ -9,3 +9,19 @@ def add (a,b):
     return a + b
 r = add(2,2)
 print(r)
+
+#lambda functions
+
+add = lambda a,b : a+b
+print(add(1,1))
+
+double = lambda n:n*2
+print(double(2))
+
+nums = [1,2,3,4]
+squares = list(map(lambda X:X*X,nums))
+print(squares)
+
+numbers = [1,2,3,4,5,6,7,8]
+odd_numbers = list(filter(lambda x:x%2 !=0,numbers))
+print(odd_numbers)
