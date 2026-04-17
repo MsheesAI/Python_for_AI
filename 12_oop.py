ba5d807b976aaecc8240e2620b30b22f892df519
MsheesAI <shees7795@gmail.com>
2026-04-17 15:15:44 +0500

    add explaination

--- 12_oop.py
+++ 12_oop.py
@@ -2,6 +2,9 @@
 
 #Object:an object is a real instance of a class
 
+#Basic class and object
+
+#class
 class Car:
     def __init__(self,company,ownership):
         self.company = company
@@ -10,6 +13,7 @@ class Car:
     def engine(self,engine="turbo v7"):
         print(f"owner is {self.ownership} and car engine is {engine}")
 
+#object
 mycar=Car("noddles","sheikh muhammad shees shoaib")
 mycar.engine("turbo v9")
 
